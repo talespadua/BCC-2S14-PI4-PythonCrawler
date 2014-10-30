bda3a5336ba8671650be2284877b069504dd2dcd
talespadua <talescpadua@gmail.com>
2014-10-30 13:58:47 -0200

    commit

--- main.py
+++ main.py
@@ -6,24 +6,19 @@ from bs4 import BeautifulSoup
 def crawler(paginas):
     pagina = 133093
     while pagina <= paginas:
-        url = "http://www.imdb.com/title/tt" + str(pagina)
+        url = "http://www.imdb.com/title/tt" + str(pagina) + "/fullcredits"
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text)
-        for link in soup.findAll('td', {'id': 'overview-top'}):
-            title = link.h1.span.string
-            time = link.time.get("datetime")
-            print("Title: " + title)
-            print("Time: " + time)
-
-            # Header do Filme
-            '''for header in link.findAll('div', {'class': 'infobar'}):
-                # Selecione todas os spans que possuem como atributo o valor genre
-                genre = header.select('span[itemprop=genre]')
-
-                # Lista todos os generos do filme
-                for item in genre:
-                    print("Genero: " + item.string)'''
+        atores = []
+        for link in soup.findAll('td', {'itemprop': 'actor'}):
+            #title = link.h1.span.string
+            #time = link.time.get("datetime")
+            #print("Title: " + title)
+            #print("Time: " + time)
+            #atores.__add__(self, nome)
+            nome = link.a.span.string
+            print(nome)
 
         pagina += 1
 
