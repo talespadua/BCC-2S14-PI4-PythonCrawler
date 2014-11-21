75ae47a6bf0dc0dc76a5e6a3210bdfeae53da639
talespadua <talescpadua@gmail.com>
2014-11-21 18:42:20 -0200

    Comecando a implementar conexao com db. Bug estranho

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ __author__ = 'tales.cpadua'
 import requests
 
 from bs4 import BeautifulSoup
-#import psycopg2
+import psycopg2
 
 def is_movie(soup):
     for sherolero in soup.findAll('div', {'class': 'infobar'}):
@@ -19,22 +19,29 @@ def is_movie(soup):
 
 
 def get_soup(url):
-    source_code = requests.get(url)
+    try:
+        source_code = requests.get(url)
+    except:
+        return None
     if source_code.status_code == 404:
         print("404")
         return None
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
+
+
     return soup
 
 def crawler(paginas):
     max_page = 133093
     running = True
-    # try:
-    #     conn = psycopg2.connect("dbname='imdbinfo' user='postgres' host='localhost' password='senha'")
-    # except:
-    #     print("I am unable to connect to the database")
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='senha'")
+    except:
+        print("I am unable to connect to the database")
 
+    cur = conn.cursor()
+    # print(cur)
     #while running:
     while paginas <= max_page:
 
@@ -43,12 +50,12 @@ def crawler(paginas):
         escritores = []
         produtores = []
         generos = []
-        nullPages = 0
-        #Here the crawler get info on main Movie page
+        null_pages = 0
 
+        #Here the crawler get info on main Movie page
         soup = get_soup("http://www.imdb.com/title/tt" + str(paginas))
         if soup is None:
-            nullPages += 1
+            null_pages += 1
             paginas += 1
             continue
 
@@ -58,83 +65,60 @@ def crawler(paginas):
 
         for dir_link in soup.findAll('h1', {'class': 'header'}):
             movie_name = dir_link.contents[1].string.strip()
+            # cur.execute('INSERT INTO "movies" (id, nome) VALUES (%s, %s)', (paginas, movie_name))
+            #cur.execute('SELECT * FROM movies')
             print(movie_name)
 
         for link in soup.findAll('a', href=True):
             if "/year/" in link['href']:
-                print(link.string)
+                launch_year = link.string
+                print(launch_year)
 
         for infobar in soup.findAll('span', {'itemprop': 'genre'}):
-            generos.append(infobar.string)
-            print(infobar.string)
+            #generos.append(infobar.string)
+            genre = infobar.string
+            print(genre)
 
         for infobar in soup.findAll('a', {'itemprop': 'url'}, href=True):
             if "country" in infobar['href']:
                 movie_country = infobar.string
                 print(movie_country)
 
+        for link in soup.findAll('div', {'itemprop': 'director'}):
+            for dir_link in link.findAll('span', {'itemprop': 'name'}):
+                director_name = dir_link.string
+                directo_profile = dir_link.parent['href']
+                print(director_name)
+                director_soup = get_soup("http://www.imdb.com/" + directo_profile)
+                if director_soup is not None:
+                    for director_link in director_soup.findAll('a', href=True):
+                        if "birth_place" in director_link['href']:
+                            place = director_link.string.split(',')
+                            birth_place = place[len(place)-1].strip()
+                            print(birth_place)
+
         for link in soup.findAll('table', {'class', 'cast_list'}):
             cast_count = 0
             for tags in link.findAll('span', {'itemprop': 'name'}):
-                print(tags.string)
+                actor_profile = tags.parent['href']
+                actor_name = tags.string
+                print(actor_name)
+                actor_soup = get_soup("http://www.imdb.com/" + actor_profile)
+                if actor_soup is not None:
+                    for actor_link in actor_soup.findAll('a', href=True):
+                        if "birth_place" in actor_link['href']:
+                            place = actor_link.string.split(',')
+                            birth_place = place[len(place)-1].strip()
+                            print(birth_place)
                 cast_count += 1
                 if cast_count > 7:
                     break
 
-        #Here the crawler get info on movie fullcredits page
+        for link in soup.findAll('span', {'itemprop': 'keywords'}):
+            keyword = link.string
+            print(keyword)
 
-        # soup = get_soup("http://www.imdb.com/title/tt" + str(paginas) + "/fullcredits")
-        #
-        # for dir_link in soup.findAll('h4', {'class': 'dataHeaderWithBorder'}):
-        #     cabecalho = dir_link.contents[0].strip()
-        #     if cabecalho == "Directed by":
-        #         print(cabecalho)
-        #         body_tag = dir_link.findNext('tbody')
-        #         for tags in body_tag.findAll('a'):
-        #             link = tags['href']
-        #             diretores.append(tags.string.strip())
-        #             print(tags.string.strip())
-        #             dir_soup = get_soup("http://www.imdb.com/" + link)
-        #             for link in dir_soup.findAll('a', href=True):
-        #                 if "birth_place" in link['href']:
-        #                     place = link.string.split(',')
-        #                     birth_place = place[len(place)-1].strip()
-        #                     print(birth_place)
-
-
-            # if cabecalho == "Writing Credits":
-            #     print(cabecalho)
-            #     body_tag = dir_link.findNext('tbody')
-            #     for tags in body_tag.findAll('a'):
-            #         escritores.append(tags.string.strip())
-            #         print(tags.string.strip())
-
-
-            # if cabecalho == "Cast":
-            #     cast_count = 0
-            #     print(cabecalho)
-            #     body_tag = dir_link.findNext('table')
-            #     for tags in body_tag.findAll('td', {'class': 'itemprop'}):
-            #         link = tags.a['href']
-            #         atores.append(tags.a.span.string.strip())
-            #         print(tags.a.span.string.strip())
-            #         dir_soup = get_soup("http://www.imdb.com/" + link)
-            #         for link in dir_soup.findAll('a', href=True):
-            #             if "birth_place" in link['href']:
-            #                 place = link.string.split(',')
-            #                 birth_place = place[len(place)-1].strip()
-            #                 print(birth_place)
-            #         cast_count += 1
-            #         if cast_count > 7:
-            #             break
-
-            # if cabecalho == "Produced by":
-            #     print(cabecalho)
-            #     body_tag = dir_link.findNext('tbody')
-            #     for tags in body_tag.findAll('a'):
-            #         produtores.append(tags.string.strip())
-            #         print(tags.string.strip())
-        nullPages = 0
+        null_pages = 0
         paginas += 1
 
 crawler(133093)
